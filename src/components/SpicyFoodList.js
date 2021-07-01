d50c954838025138ce1560214a260cb1d7255be3
Steven Vaughn <steve.h.vaughn@gmail.com>
2021-07-01 15:38:49 -0600

    completed lab

--- src/components/SpicyFoodList.js
+++ src/components/SpicyFoodList.js
@@ -3,16 +3,60 @@ import { spicyFoods, getNewSpicyFood } from "../data";
 
 function SpicyFoodList() {
   const [foods, setFoods] = useState(spicyFoods);
+  const [filterBy, setFilterBy] = useState('All')
+
+  const foodsToRender = foods.filter(food => {
+    if (filterBy === 'All') {
+      return true;
+    } else {
+      return food.cuisine === filterBy;
+    }
+  })
 
   function handleAddFood() {
     const newFood = getNewSpicyFood();
-    console.log(newFood);
+    const newFoodArray = [...foods, newFood]
+    setFoods(newFoodArray)
+  }
+
+  function handleClick(food) {
+    // Remove the clicked food
+    // const arrayWithoutClickedFood = [...foods].filter(eachFood => eachFood.id !== food.id)
+    // setFoods(arrayWithoutClickedFood)
+    
+    // Increment the heatlevel of the food by 1 when clicked
+    const foodToUpdate = foods.filter(eachFood => eachFood.id === food.id)
+    for (let info of foodToUpdate) {
+      info.heatLevel++
+    }
+    const updatedArray = [...foods]
+    setFoods(updatedArray)
+  }
+
+
+  function renderSpicyFoods() {
+      return foodsToRender.map(food => {return (
+      <li key={food.id} onClick={() => handleClick(food)}>
+        {food.name} | Heat: {food.heatLevel} | Cuisine: {food.cuisine}
+      </li>
+    )})
+  }
+
+  function filterFoods(e) {
+    setFilterBy(e.target.value)
   }
 
   return (
     <div>
       <button onClick={handleAddFood}>Add New Food</button>
-      <ul>{/* list of spicy foods */}</ul>
+      <ul>{renderSpicyFoods()}</ul>
+      <select name="filter" onChange={filterFoods}>
+        <option value="All">All</option>
+        <option value="American">American</option>
+        <option value="Sichuan">Sichuan</option>
+        <option value="Thai">Thai</option>
+        <option value="Mexican">Mexican</option>
+      </select> 
     </div>
   );
 }
